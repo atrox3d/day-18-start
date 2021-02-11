336ab452531de5a69f066fc8b4a64dcea648b6d4
atrox3d <atrox3d@gmail.com>
2021-02-11 05:22:57 +0100

    make tim draw something

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 from turtle import Turtle, Screen
 
-timmy = Turtle()
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+
+
+def draw_square():
+    """draw a sqaure"""
+    for _ in range(4):
+        tim.forward(100)
+        tim.left(90)
+
+
+def draw_alternate_line():
+    tim.left(180)
+    for _ in range(10):
+        tim.forward(10)
+        tim.up()
+        tim.forward(10)
+        tim.down()
+
+
+#draw_square()
+
+def draw_polygon(sides):
+    angle = 360 / sides
+    print(type(angle))
+    for _ in range(sides):
+        tim.forward(100)
+        tim.left(angle)
+
+
+for _ in range(3, 11):
+    draw_polygon(_)
+
+
 screen = Screen()
 screen.exitonclick()
-
 
